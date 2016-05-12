87c67482135c17120cfeb98bcc148becfee1475d
benatespina <benatespina@gmail.com>
2016-05-12 23:32:40 +0200

    Initial commit

---
+++ src/BenGorUser/SymfonyRoutingBridgeBundle/SymfonyRoutingBridgeBundle.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the BenGorUser package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BenGorUser\SymfonyRoutingBridgeBundle;
+
+use BenGorUser\UserBundle\DependentBenGorUserBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Symfony routing bridge bundle kernel class.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class SymfonyRoutingBridgeBundle extends Bundle
+{
+    use DependentBenGorUserBundle;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $this->checkDependencies(['BenGorUserBundle'], $container);
+    }
+}
