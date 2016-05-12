69092df1841766717113fbdd296189702e110212
benatespina <benatespina@gmail.com>
2016-05-13 00:06:24 +0200

    Added url generator service

---
+++ src/BenGorUser/SymfonyRoutingBridgeBundle/DependencyInjection/BenGorUserExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the BenGorUser package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BenGorUser\SymfonyRoutingBridgeBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * BenGor user bundle extension class.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class BenGorUserExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('services.yml');
+
+        $container->setParameter('bengor_user.config', $config);
+    }
+}
---
+++ src/BenGorUser/SymfonyRoutingBridgeBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the BenGorUser package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BenGorUser\SymfonyRoutingBridgeBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Symfony routing bridge bundle configuration class.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('ben_gor_user');
+
+        return $treeBuilder;
+    }
+}
