040242c7a7a6a7db7956e55c8d19e1b0ed5b6f9d
gorkalaucirica <gorka.lauzirika@gmail.com>
2016-05-13 17:45:45 +0200

    Removed configuration proccessing in Extension

--- src/BenGorUser/SymfonyRoutingBridgeBundle/DependencyInjection/SymfonyRoutingBridgeExtension.php
+++ src/BenGorUser/SymfonyRoutingBridgeBundle/DependencyInjection/SymfonyRoutingBridgeExtension.php
@@ -22,19 +22,14 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * @author Beñat Espiña <benatespina@gmail.com>
  */
-class BenGorUserExtension extends Extension
+class SymfonyRoutingBridgeExtension extends Extension
 {
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-
         $loader->load('services.yml');
-
-        $container->setParameter('bengor_user.config', $config);
     }
 }
